f8729d11d6e6d2896867fa77a9adc119d96d7b1a
HumbleGuy047 <92243872+HumbleGuy047@users.noreply.github.com>
2023-07-19 01:52:36 -0700

    Understood how to use http

--- lib/pages/select_location.dart
+++ lib/pages/select_location.dart
@@ -8,27 +8,6 @@ class SelectLocation extends StatefulWidget {
 }
 
 class _SelectLocationState extends State<SelectLocation> {
-  void getData() async {
-    // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'yoshi';
-    });
-
-    // simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'vegan, musician';
-    });
-
-    print('$username - $bio'); // Future is synchronise
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hey there!');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
